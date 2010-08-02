67553cacefed0408451980401b1b734449c1b92d
Travis Silvers <firestrand@gmail.com>
2010-08-02 17:48:47 -0400

    Partial implementation

--- SwarmOps/Optimizers/MTS.cs
+++ SwarmOps/Optimizers/MTS.cs
@@ -4,8 +4,9 @@
 /// SwarmOps on the internet: http://www.Hvass-Labs.org/
 /// ------------------------------------------------------
 
+using System;
 using System.Diagnostics;
-
+using System.Linq;
 namespace SwarmOps.Optimizers
 {
     /// <summary>
@@ -103,6 +104,8 @@ namespace SwarmOps.Optimizers
         {
             get { return _upperBound; }
         }
+
+        public int FunctionEvaluations { get; set; }
         #endregion
 
         #region Base-class overrides, Optimizer.
@@ -114,9 +117,17 @@ namespace SwarmOps.Optimizers
         {
             Debug.Assert(parameters != null && parameters.Length == Dimensionality);
 
-            // Retrieve parameter specific to GD method.
-            double stepsize = GetStepsize(parameters);
+            // Retrieve parameter specific to method.
             int numAgents = 5;
+            int numOfForeground = 3;
+            int numOfLocalSearchTest = 3;
+            int numOfLocalSearch = 100;
+            int numOfLocalSearchBest = 150;
+            double bonus1 = 10.0d;
+            double bonus2 = 1.0d;
+            var randomRangeA = new Tuple<double,double>(0.4,0.5);
+            var randomeRangeB = new Tuple<double, double>(0.1, 0.3);
+            var randomeRangeC = new Tuple<double, double>(0.0, 1.0);
 
             // Get problem-context.
             double[] lowerBound = Problem.LowerBound;
@@ -126,85 +137,121 @@ namespace SwarmOps.Optimizers
             int n = Problem.Dimensionality;
 
             // Allocate agent position and search-range.
-            double[][] x = Tools.NewMatrix(numAgents, n);					// Current position.
-            double[][] v = Tools.NewMatrix(numAgents, n);					// Gradient/velocity.
-            double[] g = new double[n];					// Best-found position.
-            double gBest = Problem.MaxFitness;
+            double[][] X = Tools.NewMatrix(numAgents, n);					// Temp Array for SOA
+            Agent[] agents = new Agent[numAgents];
+            Agent best = new Agent();
+            best.Fitness = Problem.MaxFitness;
 
-            //Algorithm variables
-            bool[] enable = new bool[numAgents];
-            bool[] improve = new bool[numAgents];
-            double[][] searchRange = Tools.NewMatrix(numAgents, n);
-            Tools.NewMatrix(numAgents, n);
+            Tools.InitializeSOA(ref X,lowerInit,upperInit);
+            //Get the fitness of each Agent
+            for (int i = 0; i < numAgents; i++)
+            {
+                agents[i].X = X[i]; //Reference Copy
+                agents[i].Fitness = Problem.Fitness(agents[i].X, best.Fitness);
+                if(agents[i].Fitness > best.Fitness)
+                {
+                    best = agents[i].Clone() as Agent;
+                }
+                //Initial Search Range
+                agents[i].SearchRange = Enumerable.Range(0, n).Select(k => (upperInit[k] - lowerInit[k]) / 2.0).ToArray();
+                agents[i].Enable = true;
+                agents[i].Improve = true;
+            }
 
-            //for (int j = 0; j < numAgents; j++)
-            //{
-            //    // Initialize agent-position in search-space.
-            //    Tools.InitializeUniform(ref x[j], lowerInit, upperInit);
-            //    Tools.InitializeUniform(ref v[j], lowerInit, upperInit);
-            //    Tools.Initialize(ref searchRange[j],0.0d);
+            double LS1_TestGrade, LS2_TestGrade, LS3_TestGrade;
+            FunctionEvaluations = 0;
+            while(Problem.RunCondition.Continue(FunctionEvaluations,best.Fitness))
+            {
+                foreach(var agent in agents)
+                {
+                    if (agent.Enable)
+                    {
+                        agent.Grade = 0.0d;
+                        LS1_TestGrade = LS2_TestGrade = LS3_TestGrade = 0.0d;
+                        for (int k = 0; k < numOfLocalSearchTest; k++)
+                        {
+                            LS1_TestGrade += LocalSearch1(agent.X, agent.SearchRange);
+                            LS2_TestGrade += LocalSearch2(agent.X, agent.SearchRange);
+                            LS3_TestGrade += LocalSearch3(agent.X, agent.SearchRange);
+                        }
+                        if(LS1_TestGrade > LS2_TestGrade && LS1_TestGrade > LS3_TestGrade)
+                        {
+                            for (int j = 0; j < numOfLocalSearch; j++)
+                            {
+                                agent.Grade += LocalSearch1(agent.X, agent.SearchRange);
+                            }
+                        }
+                        else if(LS2_TestGrade > LS1_TestGrade && LS2_TestGrade > LS3_TestGrade)
+                        {
+                            for (int j = 0; j < numOfLocalSearch; j++)
+                            {
+                                agent.Grade += LocalSearch2(agent.X, agent.SearchRange);
+                            }
+                        }
+                        else
+                        {
+                            for (int j = 0; j < numOfLocalSearch; j++)
+                            {
+                                agent.Grade += LocalSearch3(agent.X, agent.SearchRange);
+                            }
+                        }
+                    }
+                }
+                //LocalSearchBest using LocalSearch #1
+                for (int j = 0; j < numOfLocalSearchBest; j++)
+                {
+                    best.Grade += LocalSearch1(best.X, best.SearchRange);
+                }
 
-            //    enable[j] = true;
-            //    improve[j] = true;
-            //    for(int k = 0; k < n; k++)
-            //    {
-            //        searchRange[j][k] = (upperInit[k] - lowerInit[k])/2.0;
-            //    }
-            //    // Compute fitness of initial position.
-            //    // This counts as an iteration below.
-            //    double fitness = Problem.Fitness(x);
-            //}
-
-
-            
-
-            //// This is the best-found position.
-            //x.CopyTo(g, 0);
-
-            //// Trace fitness of best found solution.
-            //Trace(0, gBest);
-
-            int i = 0;
-            //for (i = 1; Problem.RunCondition.Continue(i, gBest); i++)
-            //{
-            //    // Compute gradient.
-            //    i += Problem.Gradient(x, ref v);
-
-            //    // Compute norm of gradient-vector.
-            //    double gradientNorm = Tools.Norm(v);
-
-            //    // Compute current stepsize.
-            //    double normalizedStepsize = stepsize / gradientNorm;
-
-            //    // Move in direction of steepest descent.
-            //    for (int j = 0; j < n; j++)
-            //    {
-            //        x[j] -= normalizedStepsize * v[j];
-            //    }
-
-            //    // Enforce bounds before computing new fitness.
-            //    Tools.Bound(ref x, lowerBound, upperBound);
-
-            //    // Compute new fitness.
-            //    double newFitness = Problem.Fitness(x, fitness);
-
-            //    // Update best position and fitness found in this run.
-            //    if (newFitness < fitness)
-            //    {
-            //        // Update this run's best known position.
-            //        x.CopyTo(g, 0);
-
-            //        // Update this run's best know fitness.
-            //        fitness = newFitness;
-            //    }
-
-            //    // Trace fitness of best found solution.
-            //    Trace(i, gBest);
-            //}
+                
+                foreach(var agent in agents)
+                {
+                    agent.Enable = false;
+                }
+                foreach (var agent in agents.OrderByDescending(a => a.Fitness).Take(numOfForeground))
+                {
+                    agent.Enable = true;
+                }
+                // Trace fitness of best found solution.
+                Trace(FunctionEvaluations, best.Fitness);
+            }
 
             //// Return best-found solution and fitness.
-            return new Result(g, gBest, i);
+            return new Result(best.X, best.Fitness, FunctionEvaluations);
         }
         #endregion
+        private double LocalSearch1(double[] Xi, double[] SearchRangeXi)
+        {
+            throw new NotImplementedException();
+        }
+        private double LocalSearch2(double[] Xi, double[] SearchRangeXi)
+        {
+            throw new NotImplementedException();
+        }
+        private double LocalSearch3(double[] Xi, double[] SearchRangeXi)
+        {
+            throw new NotImplementedException();
+        }
+        private class Agent : ICloneable
+        {
+            public bool Enable { get; set; }
+            public bool Improve { get; set; }
+            public double Grade { get; set; }
+            public double Fitness { get; set; }
+            public double[] SearchRange { get; set; }
+            public double[] X { get; set; }
+
+
+            public object Clone()
+            {
+                Agent clone = new Agent();
+                clone.Enable = this.Enable;
+                clone.Improve = this.Improve;
+                clone.Grade = this.Grade;
+                clone.Fitness = this.Fitness;
+                clone.SearchRange = this.SearchRange.Clone() as double[];
+                clone.X = this.X.Clone() as double[];
+            }
+        }
     }
 }
